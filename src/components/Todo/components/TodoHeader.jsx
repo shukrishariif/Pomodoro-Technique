c890e6508040fccaf3e6b47ba3ec74940fefaadb
shukri shariif <shukrishariif@gmail.com>
2024-07-27 23:01:09 +0300

    is TodoHeader

---
+++ src/components/Todo/components/TodoHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoHeader = () => {
+  return (
+    <div className="logo-container">
+      <div className="logo">T</div>
+      <h1>Todo List</h1>
+    </div>
+  );
+};
+
+export default TodoHeader;
